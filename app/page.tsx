97752ea590c2506dc72657fff2a64abe92214c66
JeetDas5 <jeet15083011@gmail.com>
2025-09-21 11:56:35 +0530

    feat: how it works section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Details from "@/components/Details";
+import HIW from "@/components/HIW";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Details />
+      <HIW />
     </main>
   );
 }
